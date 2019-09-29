ffab4762cbba663202664f63d7a9a91f2238cce6
prometheuszyx <48282803+prometheuszyx@users.noreply.github.com>
2019-09-29 11:45:35 +0800

    ‘’

---
+++ server/controller.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+
+function addControllers(router, dir) {
+	const fds = fs.readdirSync(path.join(__dirname, dir));
+	const dirs = fds.filter(f => {
+		return fs.lstatSync(path.join(__dirname, dir + "/" + f)).isDirectory();
+	});
+	const jsFiles = fds.filter(f => {
+		return (
+			fs.lstatSync(path.join(__dirname, dir + "/" + f)).isFile &&
+			f.endsWith(".js")
+		);
+	});
+	for (let d in dirs) {
+		console.log(`process dir ${dir + "/" + dirs[d]} ...`);
+		addControllers(router, dir + "/" + dirs[d]);
+	}
+	for (let f in jsFiles) {
+		console.log(`process jsfile ${dir + "/" + jsFiles[f]} ...`);
+	}
+}
+
+module.exports = addControllers;
---
+++ server/test/controller.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+const router = require("koa-router")();
+const r = require("../controller");
+
+r(router, 'controller');
