8a7e0b59b2d30b4f77b22cb74ddce347e1b8d1d7
prometheuszyx <48282803+prometheuszyx@users.noreply.github.com>
2019-10-11 16:55:49 +0800

    ‘’

--- clienttest/test1.js
+++ clienttest/test1.js
@@ -1,10 +1,10 @@
-const assert = require("chai").assert;
+const assert = require('chai').assert
 
-describe("client test1", () => {
-	it("1 = 1", () => {
-		assert.strictEqual(1, 1);
-	});
-	it("2 = 2", () => {
-		assert.strictEqual(2, 2);
-	});
-});
+describe('client test1', () => {
+    it('1 = 1', () => {
+        assert.strictEqual(1, 1)
+    })
+    it('2 = 2', () => {
+        assert.strictEqual(2, 2)
+    })
+})
