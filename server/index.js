e91b6c6ee100436b1fb5c4220be5c38fe4d9f4d6
prometheuszyx <48282803+prometheuszyx@users.noreply.github.com>
2019-10-14 15:35:33 +0800

    '

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ const CONFIG = {
     /** (number || 'session') maxAge in ms (default is 1 days) */
     /** 'session' will result in a cookie that expires when session/browser is closed */
     /** Warning: If a session cookie is stolen, this cookie will never expire */
-    maxAge: 60000,
+    maxAge: 'session',
     autoCommit: true /** (boolean) automatically commit headers (default true) */,
     overwrite: true /** (boolean) can overwrite or not (default true) */,
     httpOnly: true /** (boolean) httpOnly or not (default true) */,
