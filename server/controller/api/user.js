429bd52aad612c0d2a888c532986679945dbd0cb
prometheuszyx <48282803+prometheuszyx@users.noreply.github.com>
2019-10-18 15:04:45 +0800

    '

--- server/controller/api/user.js
+++ server/controller/api/user.js
@@ -12,7 +12,7 @@ userRouter.get('/', async (ctx, next) => {
 })
 
 // 返回当前用户能够访问的路径
-userRouter.get('/authnavs', ctx => {
+userRouter.get('/permissions', ctx => {
     const u = ctx.state.currentUser
     ctx.state = 200
     ctx.response.type = 'text/json'
