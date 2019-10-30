292dc7bee5d3370ac45d43be7c290423c2220ea1
prometheuszyx <48282803+prometheuszyx@users.noreply.github.com>
2019-10-30 16:03:35 +0800

    '

--- server/index.ts
+++ server/index.ts
@@ -147,7 +147,12 @@ app.use(async (ctx: any, next) => {
     ctx.status = 200
     ctx.respond = false // Bypass Koa's built-in response handling
     ctx.req.ctx = ctx // This might be useful later on, e.g. in nuxtServerInit or with nuxt-stash
-    nuxt.render(ctx.req, ctx.res)
+    try{
+        nuxt.render(ctx.req, ctx.res)
+    }catch(error) {
+        console.log('nuxt throw error:!!!!!!!!!!!!!!')
+        console.log(error)
+    }
 })
 
 // app.listen(56556)
