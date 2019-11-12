e9de0b80530eb81be28ae96f4da6915c2fe3d139
ucaesar <ucaesar@qq.com>
2019-11-12 16:45:49 +0800

    1

---
+++ components/superadmin/ResourceGroupTable.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <v-data-table :headers="headers" class="elevation-1"></v-data-table>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component
+class ResourceGroupTable extends Vue {
+    headers = [
+        {
+            text: 'Name',
+            value: 'name',
+            sortable: false
+        },
+        {
+            text: 'Description',
+            value: 'description',
+            sortable: false
+        }
+    ];
+}
+
+export default ResourceGroupTable;
+</script>
+
+<style>
+</style>
--- pages/superadmin/ResourceGroup.vue
+++ pages/superadmin/ResourceGroup.vue
@@ -3,7 +3,13 @@
 </template>
 
 <script lang="ts">
+/* 
+    api/resource-group/:id/children
+    获取指定id的ResourceGroup的所有子group, id=1的group是所有子group的根
+*/
+import consola from 'consola';
 import { Component, Vue } from 'nuxt-property-decorator';
+import { NuxtAxiosInstance } from '@nuxtjs/axios';
 
 interface ResourceGroup {
     id: number;
@@ -12,14 +18,25 @@ interface ResourceGroup {
 }
 
 interface ResourceGroupList {
-    result: ResourceGroup[],
-    total: number
+    result: ResourceGroup[];
+    total: number;
 }
 
 @Component({
     layout: 'admin'
 })
-class ResourceGroupManager extends Vue {}
+class ResourceGroupManager extends Vue {
+    async asyncData({ $axios }: { $axios: NuxtAxiosInstance }) {
+        const url = 'api/resource-group/1/children';
+        let resourceGroups: ResourceGroupList = { result: [], total: 0 };
+        try {
+            resourceGroups = (await $axios.$get(url)) as ResourceGroupList;
+        } catch (error) {
+            consola.error(`error from get(${url})`, error);
+        }
+        return { resourceGroups };
+    }
+}
 export default ResourceGroupManager;
 </script>
 
