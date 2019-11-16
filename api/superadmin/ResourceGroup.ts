9aad86beb69d798b4acb93f4c7e7e9c34489fac9
ucaesar <ucaesar@qq.com>
2019-11-16 22:02:58 +0800

    1

--- api/superadmin/ResourceGroup.ts
+++ api/superadmin/ResourceGroup.ts
@@ -1,8 +1,7 @@
-import { NuxtAxiosInstance } from "@nuxtjs/axios";
-import Vue from 'vue'
+import { NuxtAxiosInstance } from '@nuxtjs/axios';
+import Vue from 'vue';
 
 import { TableDataFromServer } from '@/api/admin';
-import consolaGlobalInstance from "consola";
 
 export interface ResourceGroup {
     id: number;
@@ -10,21 +9,24 @@ export interface ResourceGroup {
     description: string;
 }
 
-// 
-
-export async function readResourceGroups({ page, itemsPerPage }: { page: number, itemsPerPage: number }): Promise<TableDataFromServer> {
+export async function readResourceGroups({
+    page,
+    itemsPerPage
+}: {
+    page: number;
+    itemsPerPage: number;
+}): Promise<TableDataFromServer> {
     const url = 'api/resource-group/1/children';
-    const $axios: NuxtAxiosInstance = Vue.prototype.$nuxt.$options.$axios
-    let resourceGroups: TableDataFromServer = { result: [], total: 0 }
+    const $axios: NuxtAxiosInstance = Vue.prototype.$nuxt.$options.$axios;
+    let resourceGroups: TableDataFromServer = { result: [], total: 0 };
 
     try {
         resourceGroups = (await $axios.$get(url, {
             params: { start: (page - 1) * itemsPerPage, count: itemsPerPage }
         })) as TableDataFromServer;
-    }
-    catch (error) {
-        throw error
+    } catch (error) {
+        throw error;
     }
 
-    return resourceGroups
+    return resourceGroups;
 }
