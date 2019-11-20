1ee0d31510e3d2ff561d70d4b1ad2327326eff62
prometheuszyx <48282803+prometheuszyx@users.noreply.github.com>
2019-11-20 11:13:29 +0800

    '

--- server/controller/api/resource.ts
+++ server/controller/api/resource.ts
@@ -5,7 +5,7 @@ import { Op } from 'sequelize';
 import _ from 'lodash';
 
 // 获取Resource列表
-resourceRouter.get('/', async (ctx, next) => {
+resourceRouter.get('/', async ctx => {
     // 获取分页参数
     const start = ctx.request.query.start;
     const num = ctx.request.query.count;
@@ -30,6 +30,39 @@ resourceRouter.get('/', async (ctx, next) => {
         results,
         total
     };
+});
+
+// 添加一个resource
+resourceRouter.post('/', async ctx => {
+    const name: string = (ctx.req as any).body.name || '';
+    const description: string = (ctx.req as any).body.description || '';
+    const url: string = (ctx.req as any).body.url || '';
+    const action: string = (ctx.req as any).body.action || '';
+    if (name === '' || url === '' || action === '') {
+        ctx.response.status = 401;
+        ctx.response.body = 'name url action are all needed';
+        return;
+    }
+    //检查name是否存在
+    if ((await Resource.count({ where: { name } })) > 0) {
+        ctx.response.status = 401;
+        ctx.response.body = 'name exists';
+        return;
+    }
+    if ((await Resource.count({ where: { url, action } })) > 0) {
+        ctx.response.status = 401;
+        ctx.response.body = 'url action combination exists';
+        return;
+    }
+    const newresource = await Resource.create({
+        name,
+        description,
+        url,
+        action
+    });
+    ctx.response.type = 'text/json';
+    ctx.response.status = 200;
+    ctx.response.body = newresource;
 });
 
 export default resourceRouter;
---
+++ server/test/resource.ts
@@ -0,0 +1,55 @@
+import chai from 'chai';
+const expect = require('chai').expect;
+import chaiHttp from 'chai-http';
+import { initConnect } from './index';
+import connect from 'connect';
+import * as http from 'http';
+import Resource from '../model/ResourceResourceGroup';
+
+chai.use(chaiHttp);
+
+describe('Resource API test', () => {
+    let app;
+    let server: http.Server;
+    let req: ChaiHttp.Agent;
+
+    before(async function() {
+        app = connect();
+        await initConnect(app);
+        server = app.listen(56556);
+        // await connectdb(sequelize);
+    });
+
+    after(async function() {
+        server.close();
+    });
+
+    beforeEach(function() {
+        req = chai.request.agent(server);
+    });
+
+    afterEach(function() {
+        req.close();
+    });
+
+    it('test add a resource using restful api', async () => {
+        let res = await req
+            .post('/api/user/login')
+            .type('json')
+            .send({
+                username: 'superadmin',
+                password: 'superadmin'
+            });
+        // expect(res).to.have.cookie("koa:sess");
+        res = await req
+            .post('/api/resource/')
+            .type('json')
+            .send({
+                name: 'r2',
+                description: 'test resource 1',
+                url: '/api/resource-group/',
+                action: 'GET'
+            });
+        expect(res).to.have.status(200);
+    });
+});
