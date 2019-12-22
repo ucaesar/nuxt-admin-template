04b8ff8e16578a931183da4ac5c06985445802fd
ucaesar <ucaesar@qq.com>
2019-12-22 16:59:44 +0800

    1

---
+++ components/common/CrudTable/CrudTable.vue
@@ -0,0 +1,15 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator';
+
+@Component
+class CrudTable extends Vue {}
+
+export default CrudTable;
+</script>
+
+<style>
+</style>
