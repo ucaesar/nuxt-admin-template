6de26632459a5cb8640b1fbe7eb4c72e318a3f34
prometheuszyx <48282803+prometheuszyx@users.noreply.github.com>
2019-12-27 12:14:04 +0800

    ‘’

--- server/controller/api/user.ts
+++ server/controller/api/user.ts
@@ -7,6 +7,43 @@ import Role from '../../model/Role';
 import { Op, where } from 'sequelize';
 import getEnforcer from '../../lib/enforcer';
 
+// 返回当前用户能够访问的路径
+userRouter.get('/permissions', ctx => {
+    const u = ctx.state.currentUser;
+    ctx.state = 200;
+    ctx.response.type = 'text/json';
+    ctx.response.body = u.authNavs;
+});
+
+const authenticator = new SessionAuthenticator();
+userRouter.post('/login', async (ctx, next) => {
+    // ctx.session.x_session = encode('2e592d50-d535-11e9-881c-31c34ad71a1b');
+    // ctx.session.username = "aaa";
+    // ctx.state.currentUser = { username: "aaa" };
+    await authenticator.login(ctx);
+    // 根据currentUser的内容返回登陆是否成功的结果
+    ctx.response.type = 'text/json';
+    const loginResult =
+        ctx.state.currentUser && ctx.state.currentUser.username !== 'anonymous';
+    // 登陆成功，返回200状态，否则返回401
+    ctx.status = loginResult ? 200 : 401;
+    const url = loginResult ? '/superadmin' : '/';
+    ctx.response.body = {
+        redirect: url
+    };
+    // await next()
+});
+
+userRouter.post('/logout', ctx => {
+    authenticator.logout(ctx);
+    ctx.status = 200;
+    ctx.response.type = 'text/json';
+    ctx.response.body = {
+        redirect: '/login'
+    };
+    // await next()
+});
+
 /**
  * 获取User列表
  * url: /api/user
@@ -127,43 +164,6 @@ userRouter.put('/:id', async ctx => {
         ctx.response.status = 404;
         ctx.response.body = 'not found';
     }
-});
-
-// 返回当前用户能够访问的路径
-userRouter.get('/permissions', ctx => {
-    const u = ctx.state.currentUser;
-    ctx.state = 200;
-    ctx.response.type = 'text/json';
-    ctx.response.body = u.authNavs;
-});
-
-const authenticator = new SessionAuthenticator();
-userRouter.post('/login', async (ctx, next) => {
-    // ctx.session.x_session = encode('2e592d50-d535-11e9-881c-31c34ad71a1b');
-    // ctx.session.username = "aaa";
-    // ctx.state.currentUser = { username: "aaa" };
-    await authenticator.login(ctx);
-    // 根据currentUser的内容返回登陆是否成功的结果
-    ctx.response.type = 'text/json';
-    const loginResult =
-        ctx.state.currentUser && ctx.state.currentUser.username !== 'anonymous';
-    // 登陆成功，返回200状态，否则返回401
-    ctx.status = loginResult ? 200 : 401;
-    const url = loginResult ? '/superadmin' : '/';
-    ctx.response.body = {
-        redirect: url
-    };
-    // await next()
-});
-
-userRouter.post('/logout', ctx => {
-    authenticator.logout(ctx);
-    ctx.status = 200;
-    ctx.response.type = 'text/json';
-    ctx.response.body = {
-        redirect: '/login'
-    };
-    // await next()
 });
 
 // 暂时统一到api/login接口去
