54a5d93cfd823c02acdac59f6e3c13677548b554
ucaesar <ucaesar@qq.com>
2019-12-31 12:33:38 +0800

    1

--- components/user/Register.vue
+++ components/user/Register.vue
@@ -1,5 +1,29 @@
 <template>
-    <div></div>
+    <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+            <v-col cols="12">
+                <v-card class="elevation-12">
+                    <v-toolbar color="primary" dark flat>
+                        <v-btn icon><v-icon>mdi-account</v-icon></v-btn>
+                        <v-toolbar-title>
+                            {{ $t('register.toolbarText') }}
+                        </v-toolbar-title>
+                    </v-toolbar>
+                    <v-card-text></v-card-text>
+                    <v-card-actions>
+                        <div class="flex-grow-1"></div>
+                        <v-btn
+                            color="primary"
+                            class="mb-1"
+                            :loading="loading"
+                            @click="onSubmit"
+                            >{{ $t('register.submitButtonText') }}</v-btn
+                        >
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script lang="ts">
