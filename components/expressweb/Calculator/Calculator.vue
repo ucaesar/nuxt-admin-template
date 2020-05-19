c460dfbd7f873bf1715b116b29724c2171810e89
ucaesar <ucaesar@qq.com>
2020-05-19 17:33:36 +0800

    1

---
+++ components/expressweb/Calculator/Calculator.vue
@@ -0,0 +1,17 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Ref } from 'nuxt-property-decorator';
+
+import CountryInput from '@/components/expressweb/Address/CountryInput.vue';
+import ProvinceInput from '@/components/expressweb/Address/ProvinceInput.vue';
+import CityInput from '@/components/expressweb/Address/CityInput.vue';
+
+@Component
+class Calculator extends Vue {}
+</script>
+
+<style>
+</style>
