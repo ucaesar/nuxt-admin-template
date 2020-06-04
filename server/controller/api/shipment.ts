764daaac43addf015c556a443e66d5271904af08
prometheuszyx <48282803+prometheuszyx@users.noreply.github.com>
2020-06-04 09:30:32 +0800

    '

--- server/controller/api/shipment.ts
+++ server/controller/api/shipment.ts
@@ -167,76 +167,7 @@ shipmentRouter.post('/create', async ctx => {
             ]
         }
     };
-    // const res = await ship(requestedshipment2);
-    const res = await ship({
-        RequestedShipment: {
-            ShipTimestamp: new Date(
-                date.getTime() + 24 * 60 * 60 * 1000
-            ).toISOString(),
-            DropoffType: 'REGULAR_PICKUP',
-            ServiceType: 'FEDEX_GROUND',
-            PackagingType: 'YOUR_PACKAGING',
-            Shipper: {
-                Contact: {
-                    PersonName: 'Sender Name',
-                    CompanyName: 'Company Name',
-                    PhoneNumber: '5555555555'
-                },
-                Address: {
-                    StreetLines: ['Address Line 1'],
-                    City: 'Collierville',
-                    StateOrProvinceCode: 'TN',
-                    PostalCode: '38017',
-                    CountryCode: 'US'
-                }
-            },
-            Recipient: {
-                Contact: {
-                    PersonName: 'Recipient Name',
-                    CompanyName: 'Company Receipt Name',
-                    PhoneNumber: '5555555555'
-                },
-                Address: {
-                    StreetLines: ['Address Line 1'],
-                    City: 'Charlotte',
-                    StateOrProvinceCode: 'NC',
-                    PostalCode: '28202',
-                    CountryCode: 'US',
-                    Residential: false
-                }
-            },
-            ShippingChargesPayment: {
-                PaymentType: 'SENDER',
-                Payor: {
-                    ResponsibleParty: {
-                        AccountNumber: fedex.options.account_number
-                    }
-                }
-            },
-            LabelSpecification: {
-                LabelFormatType: 'COMMON2D',
-                ImageType: 'PDF',
-                LabelStockType: 'PAPER_4X6'
-            },
-            PackageCount: '1',
-            RequestedPackageLineItems: [
-                {
-                    SequenceNumber: 1,
-                    GroupPackageCount: 1,
-                    Weight: {
-                        Units: 'LB',
-                        Value: '50.0'
-                    },
-                    Dimensions: {
-                        Length: 108,
-                        Width: 5,
-                        Height: 5,
-                        Units: 'IN'
-                    }
-                }
-            ]
-        }
-    });
+    const res = await ship(requestedshipment);
     result = {
         label:
             res.CompletedShipmentDetail.CompletedPackageDetails[0].Label
