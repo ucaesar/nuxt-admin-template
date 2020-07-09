8f1e425004a79cc34750d2a5f4aa00972c5778c6
ucaesar <ucaesar@qq.com>
2020-07-09 17:12:02 +0800

    1

---
+++ pages/account/shipment.vue
@@ -0,0 +1,25 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, namespace } from 'nuxt-property-decorator';
+
+const adminStore = namespace('admin');
+
+@Component({
+    layout: 'admin'
+})
+class ShipmentPage extends Vue {
+    @adminStore.Mutation('SET_PAGETITLE') setPageTitle;
+    
+    mounted() {
+        this.setPageTitle(this.$t('expressweb.pageTitle.account.shipment'));
+    }
+}
+
+export default ShipmentPage;
+</script>
+
+<style>
+</style>
