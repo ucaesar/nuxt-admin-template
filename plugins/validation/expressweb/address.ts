57f758212b1edef4d34bcb242d80451bdd2ea32e
ucaesar <ucaesar@qq.com>
2020-07-17 23:53:04 +0800

    1

--- plugins/validation/expressweb/address.ts
+++ plugins/validation/expressweb/address.ts
@@ -8,11 +8,9 @@ export function provinceCode(province: IProvince | string) {
     console.log('typeof province, ', typeof province);
     if (typeof province === 'string') return false;
 
-    const ps = provinces[province.countryCode];
-
-    for (const p of ps) {
+    for (const p of provinces[province.countryCode]) {
         console.log('p! ', p);
-        if ((p as any).code === province.code) return true;
+        if (p.code === province.code) return true;
     }
     return false;
 }
